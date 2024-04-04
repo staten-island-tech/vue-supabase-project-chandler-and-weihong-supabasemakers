92601c2384781c3248bc608389da098880f11e7a
chandlermay <133597587+chandlermay@users.noreply.github.com>
2024-04-04 13:13:24 -0400

    set up supabase file and test ran with countries code

---
+++ project/src/lib/supabaseClient.js
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient('https://gpzymqnrxhglplnvdgao.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdwenltcW5yeGhnbHBsbnZkZ2FvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIyNTAwNjYsImV4cCI6MjAyNzgyNjA2Nn0.H932C99DTX-FnVnNHaTWSy2X9Rqfe6Et11ySd4yB-hU')
