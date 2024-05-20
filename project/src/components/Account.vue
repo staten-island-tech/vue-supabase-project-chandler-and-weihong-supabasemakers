5591ce4e457c3660d4c15a5b76d67ec07907677a
chandlermay <133597587+chandlermay@users.noreply.github.com>
2024-05-20 12:46:45 -0400

    whys it not working

--- project/src/components/Account.vue
+++ project/src/components/Account.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { supabase } from '../supabase'
 import { onMounted, ref, toRefs } from 'vue'
-// import { Avatar } from './Avatar.vue'
+ import { uploadAvatar } from './Avatar.vue'
 
 const props = defineProps(['session'])
 const { session } = toRefs(props)
@@ -78,7 +78,7 @@ async function signOut() {
 
 <template>
   <form class="form-widget" @submit.prevent="updateProfile">
-    <!-- <Avatar v-model:path="avatar_url" @upload="updateProfile" size="10" /> -->
+    <uploadAvatar v-model:path="avatar_url" @upload="updateProfile" size="10" />
     <div>
       <label for="email">Email</label>
       <input id="email" type="text" :value="session.user.email" disabled />
