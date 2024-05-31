87fe9bf98465eb5e513960ec5911ed4991d3560b
koolaids21 <weihong2398@gmail.com>
2024-05-30 23:31:01 -0400

    a

--- project/src/components/avatar.vue
+++ project/src/components/avatar.vue
@@ -1,53 +1,57 @@
 <script setup>
-import { ref, toRefs, watch } from 'vue'
-import { supabase } from '../supabase'
-export { downloadImage } 
+import { defineProps, defineEmits, ref, toRefs, watch } from "vue";
+import { supabase } from "../supabase";
+export { downloadImage };
 
-const prop = defineProps(['path', 'size'])
-const { path, size } = toRefs(prop)
+const prop = defineProps(["path", "size"]);
+const { path, size } = toRefs(prop);
 
-const emit = defineEmits(['upload', 'update:path'])
-const uploading = ref(false)
-const src = ref('')
-const files = ref()
+const emit = defineEmits(["upload", "update:path"]);
+const uploading = ref(false);
+const src = ref("");
+const files = ref();
 
 const downloadImage = async () => {
   try {
-    const { data, error } = await supabase.storage.from('avatars').download(path.value)
-    if (error) throw error
-    src.value = URL.createObjectURL(data)
+    const { data, error } = await supabase.storage
+      .from("avatars")
+      .download(path.value);
+    if (error) throw error;
+    src.value = URL.createObjectURL(data);
   } catch (error) {
-    console.error('Error downloading image: ', error.message)
+    console.error("Error downloading image: ", error.message);
   }
-}
+};
 
 const uploadAvatar = async (evt) => {
-  files.value = evt.target.files
+  files.value = evt.target.files;
   try {
-    uploading.value = true
+    uploading.value = true;
     if (!files.value || files.value.length === 0) {
-      throw new Error('You must select an image to upload.')
+      throw new Error("You must select an image to upload.");
     }
 
-    const file = files.value[0]
-    const fileExt = file.name.split('.').pop()
-    const filePath = `${Math.random()}.${fileExt}`
+    const file = files.value[0];
+    const fileExt = file.name.split(".").pop();
+    const filePath = `${Math.random()}.${fileExt}`;
 
-    const { error: uploadError } = await supabase.storage.from('avatars').upload(filePath, file)
+    const { error: uploadError } = await supabase.storage
+      .from("avatars")
+      .upload(filePath, file);
 
-    if (uploadError) throw uploadError
-    emit('update:path', filePath)
-    emit('upload')
+    if (uploadError) throw uploadError;
+    emit("update:path", filePath);
+    emit("upload");
   } catch (error) {
-    alert(error.message)
+    alert(error.message);
   } finally {
-    uploading.value = false
+    uploading.value = false;
   }
-}
+};
 
 watch(path, () => {
-  if (path.value) downloadImage()
-})
+  if (path.value) downloadImage();
+});
 </script>
 
 <template>
@@ -59,11 +63,15 @@ watch(path, () => {
       class="avatar image"
       :style="{ height: size + 'em', width: size + 'em' }"
     />
-    <div v-else class="avatar no-image" :style="{ height: size + 'em', width: size + 'em' }" />
+    <div
+      v-else
+      class="avatar no-image"
+      :style="{ height: size + 'em', width: size + 'em' }"
+    />
 
     <div :style="{ width: size + 'em' }">
       <label class="button primary block" for="single">
-        {{ uploading ? 'Uploading ...' : 'Upload' }}
+        {{ uploading ? "Uploading ..." : "Upload" }}
       </label>
       <input
         style="visibility: hidden; position: absolute"
