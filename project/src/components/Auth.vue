55fffb2fb8ecf012bdfc431235ab1b46b5d9e338
chandlermay <133597587+chandlermay@users.noreply.github.com>
2024-06-03 12:38:42 -0400

    change to email and passwor

--- project/src/components/Auth.vue
+++ project/src/components/Auth.vue
@@ -1,44 +1,30 @@
-<script setup>
-import { ref } from 'vue'
-import { supabase } from '../supabase'
-
-const loading = ref(false)
-const email = ref('')
-
-const handleLogin = async () => {
-  try {
-    loading.value = true
-    const { error } = await supabase.auth.signInWithOtp({
-      email: email.value,
-    })
-    if (error) throw error
-    alert('Check your email for the login link!')
-  } catch (error) {
-    if (error instanceof Error) {
-      alert(error.message)
-    }
-  } finally {
-    loading.value = false
-  }
-}
-</script>
-
 <template>
-  <form class="row flex-center flex" @submit.prevent="handleLogin">
-    <div class="col-6 form-widget">
-      <h1 class="header">InstOgram</h1>
-      <p class="description">Sign in via magic link with your email below</p>
-      <div>
-        <input class="inputField" required type="email" placeholder="Your email" v-model="email" />
-      </div>
-      <div>
-        <input
-          type="submit"
-          class="button block"
-          :value="loading ? 'Loading' : 'Send magic link'"
-          :disabled="loading"
-        />
-      </div>
-    </div>
-  </form>
+  <div>
+    <form @submit.prevent="signIn">
+      <input v-model="email" type="email" placeholder="Email" required />
+      <input
+        v-model="password"
+        type="password"
+        placeholder="Password"
+        required
+      />
+      <button type="submit">Sign In</button>
+    </form>
+  </div>
 </template>
+
+<script setup>
+import { ref } from "vue";
+import { supabase } from "../supabase";
+
+const email = ref("");
+const password = ref("");
+
+const signIn = async () => {
+  const { error } = await supabase.auth.signInWithPassword({
+    email: email.value,
+    password: password.value,
+  });
+  if (error) console.error("Error signing in:", error.message);
+};
+</script>
