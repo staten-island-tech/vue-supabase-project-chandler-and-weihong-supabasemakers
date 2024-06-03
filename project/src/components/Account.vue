7a3ee6b9625ad205051d2a4ca190c8246e1162e0
chandlermay <133597587+chandlermay@users.noreply.github.com>
2024-06-03 13:12:45 -0400

    not working

--- project/src/components/Account.vue
+++ project/src/components/Account.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h2>Welcome, {{ niceName }}</h2>
+    <h2>Welcome, {{ email.value }}</h2>
     <button @click="signOut">Sign Out</button>
   </div>
 </template>
@@ -8,11 +8,12 @@
 <script setup>
 import { supabase } from "../supabase";
 import { defineProps } from "vue";
-
+import {email} from "./auth.vue";
 
 
 const props = defineProps({
   session: Object,
+  email: Object,
 });
 
 const niceName = session.user.email.toString
