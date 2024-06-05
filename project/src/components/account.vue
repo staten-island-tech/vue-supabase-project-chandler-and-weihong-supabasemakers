77f62a44bbde8c9e47369ffbb75b33a799aa3215
weihong <weihong2398@gmail.com>
2024-06-05 13:17:03 -0400

    a little progress in creating new posts

--- project/src/components/account.vue
+++ project/src/components/account.vue
@@ -3,16 +3,32 @@
     <h2>Welcome, {{ session.user.email }}</h2>
     <button @click="signOut">Sign Out</button>
   </div>
+  <div>
+    <form>
+      <button @click="newpost" >Createpost</button>
+      <input type="text" v-model="postcontent">
+    </form>
+  </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
 import { supabase } from "../supabase";
 import { defineProps } from "vue";
+
+const postcontent = ref("");
 
 const props = defineProps({
   session: Object,
 });
 
+
+const newpost = async () => {await supabase
+  .from('posts')
+  .insert({ contents, postcontent  })
+}
+
+  
 const signOut = async () => {
   await supabase.auth.signOut();
 };
