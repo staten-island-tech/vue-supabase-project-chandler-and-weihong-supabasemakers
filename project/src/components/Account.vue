3d22225ffd0044fa05199fd84ce57860a0740435
chandlermay <133597587+chandlermay@users.noreply.github.com>
2024-06-06 22:19:56 -0400

    Update Account.vue

--- project/src/components/Account.vue
+++ project/src/components/Account.vue
@@ -6,22 +6,12 @@
     <h2 v-else>Loading...</h2>
     <button v-if="sessionStore.session" @click="signOut">Sign Out</button>
   </div>
-  <div>
-    <form>
-      <button @click="newpost">Createpost</button>
-      <input type="text" v-model="postcontent" />
-    </form>
-  </div>
 </template>
 
 <script setup>
 import { useSessionStore } from "../stores/pinia";
 
 const sessionStore = useSessionStore();
-
-const newpost = async () => {
-  await supabase.from("posts").insert({ contents, postcontent });
-};
 
 const signOut = async () => {
   await sessionStore.signOut();
