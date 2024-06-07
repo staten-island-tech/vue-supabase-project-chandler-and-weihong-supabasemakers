7d56aa4f7589eb3f330c4e8e2d108a4c5f760a04
koolaids21 <weihong2398@gmail.com>
2024-06-06 23:03:49 -0400

    Update index.js

--- project/src/router/index.js
+++ project/src/router/index.js
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import SignUp from "../components/signup.vue";
 import SignIn from "../components/signin.vue";
-import Account from "../components/account.vue";
+import Account from "../components/Account.vue";
 
 const routes = [
   { path: "/signup", name: "signup", component: SignUp },
   { path: "/signin", name: "signin", component: SignIn },
-  { path: "/account", name: "account", component: Account, props: true },
+  { path: "/account", name: "Account", component: Account, props: true },
 ];
 
 const router = createRouter({
